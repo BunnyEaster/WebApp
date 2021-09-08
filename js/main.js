76f8bd4e7b646e3f8e3800df1df5f2522995dfc8
bunnyeaster <90109137+BunnyEaster@users.noreply.github.com>
2021-09-08 16:26:03 +0530

    try 2

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(".slider").slick({
+  infinite: true,
+  dots: true,
+  arrows: false,
+  autoplay: true,
+  autoplaySpeed: 1000,
+  fade: true,
+  fadeSpeed: 1000,
+  //   slidesToShow: 2,s
+});
