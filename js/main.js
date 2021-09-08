6e94e7833cb7b61147bfab3c7a6c019e65768194
bunnyeaster <90109137+BunnyEaster@users.noreply.github.com>
2021-09-09 00:29:15 +0530

    inner witdh

--- js/main.js
+++ js/main.js
@@ -8,3 +8,28 @@ $(".slider").slick({
   fadeSpeed: 1000,
   //   slidesToShow: 2,s
 });
+
+// header
+// var wrap = $(".bg-img");
+
+// $("header").on("scroll", () => {
+//   console.log("this.scrollTop");
+//   if (this.scrollTop > window.innerHeight) {
+//     $(".bg-img").addClass("fix-search");
+//   } else {
+//     $(".bg-img").removeClass("fix-search");
+//   }
+// });
+
+window.onscroll = function () {
+  //   if (window.pageYOffset > window.innerHeight) {
+  //     $(".bg-img").addClass("fix-search");
+  //   } else {
+  //     $(".bg-img").removeClass("fix-search");
+  //   }
+  if (window.pageYOffset > window.innerWidth - window.innerHeight) {
+    $(".bg-img").addClass("fix-search");
+  } else {
+    $(".bg-img").removeClass("fix-search");
+  }
+};
