c04a928121fe312b10a4616061b5110e7ae1f706
bunnyeaster <90109137+BunnyEaster@users.noreply.github.com>
2021-09-09 17:38:52 +0530

    ok

--- js/main.js
+++ js/main.js
@@ -38,3 +38,31 @@ if (window.innerWidth < window.innerHeight) {
   $(".bg-img").addClass("fix-search");
   $(".fix-search").css("background-position", "top");
 }
+
+function navswitch() {
+  var x = document.getElementById("nav");
+  var x2 = document.getElementById("wallet-btn");
+  var burger = document.getElementById("fa-bars");
+  var close = document.getElementById("fa-times");
+  // var x3 = document.getElementById("social");
+  // var navClass = document.getElementsByClassName("navbar");
+  // var hrg = document.getElementById("hrgrad");
+
+  if (x.style.display === "flex") {
+    x.style.display = "none";
+    x2.style.display = "none";
+    burger.style.display = "block";
+    close.style.display = "none";
+    // hrg.style.display = "none";
+    // x3.style.display = "none";
+    // navClass.style.flexDirection = "row";
+  } else {
+    x.style.display = "flex";
+    x2.style.display = "flex";
+    burger.style.display = "none";
+    close.style.display = "block";
+    // hrg.style.display = "block";
+    // x3.style.display = "flex";
+    // navClass.style.flexDirection = "column";
+  }
+}
